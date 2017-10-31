35fea5f381071f555b5d23f438bd5b9acfe563d4
Michael Delaney <michael@fusepilot.com>
2017-10-30 17:06:33 -0700

    simplify

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,31 +1,28 @@
+// define a field on certain pages
 function conditionalCreateNodeField({ node, createNodeField }) {
-  if (node.internal.type == 'SitePage') {
-    // define a field on certain pages
-    if (node.path == '/page-a/') {
-      createNodeField({
-        node,
-        name: 'foo',
-        value: 'bar a',
-      })
-    }
-  }
-}
-
-function unconditionalCreateNodeField({ node, createNodeField }) {
-  if (node.internal.type == 'SitePage') {
-    // define a field on all pages
+  if (node.path == '/page-a/') {
     createNodeField({
       node,
       name: 'foo',
-      value: 'bar',
+      value: 'bar a',
     })
   }
+}
+
+// define a field on all pages
+function unconditionalCreateNodeField({ node, createNodeField }) {
+  createNodeField({
+    node,
+    name: 'foo',
+    value: 'bar',
+  })
 }
 
 exports.onCreateNode = async ({ node, boundActionCreators }) => {
   const { createNodeField } = boundActionCreators
 
-  conditionalCreateNodeField({ node, createNodeField }) // throws GraphQL Error
-
-  // unconditionalCreateNodeField({ node, createNodeField }) // no error
+  if (node.internal.type == 'SitePage') {
+    conditionalCreateNodeField({ node, createNodeField }) // throws GraphQL Error
+    // unconditionalCreateNodeField({ node, createNodeField }) // no error
+  }
 }
